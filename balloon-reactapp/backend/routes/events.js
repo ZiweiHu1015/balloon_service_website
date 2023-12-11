ca5f221041b45007740fcb2c8d45ab5a58aacf0f
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2023-12-11 14:38:49 -0500

    ziwei events backend

--- balloon-reactapp/backend/routes/events.js
+++ balloon-reactapp/backend/routes/events.js
@@ -2,53 +2,57 @@
 const router = require('express').Router();
 let Events = require('../models/events.model');//need to require the model
 
-router.route('/').get((req,res) =>{
-    Events.find() //find all events in database
-        .then(events => res.json(events))
-        .catch(err => res.status(400).json('Error: '+err));
-});//first route handles http get request
+    //get all events 
+    router.route('/').get((req,res) =>{
+        Events.find() //find all events in database
+            .then(events => res.json(events))
+            .catch(err => res.status(400).json('Error: '+err));
+    });//first route handles http get request
 
-router.route('/add').post((req,res) =>{
-    const eventname = req.body.eventname;
-    const description = req.body.description;
-    const price = Number(req.body.price);
-
-    const newEvents = new Events({
-        eventname,  
-        description,
-        price,
-    });
-
-    newEvents.save()
-        .then(()=> res.json('Events added!'))
-        .catch(err => res.status(400).json('Error: '+ err));
-    });//second route handles http post request
-
+    //get event by ID
     router.route('/:id').get((req, res) => {
         Events.findById(req.params.id)
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error: ' + err));
     });
 
+    //create new event
+    router.route('/add').post((req,res) =>{
+        const eventname = req.body.eventname;
+        const description = req.body.description;
+        const price = Number(req.body.price);
+
+        const newEvents = new Events({
+            eventname,  
+            description,
+            price,
+        });
+    newEvents.save()
+        .then(()=> res.json('Events added!'))
+        .catch(err => res.status(400).json('Error: '+ err));
+    });//second route handles http post request
+
+    //update event
+    router.route('/update/:id').post((req, res) => {
+        Events.findById(req.params.id)
+        .then(events => {
+        events.eventsname = req.body.eventsname;
+        events.description = req.body.description;
+        events.price = Number(req.body.price);
+    
+        events.save()
+            .then(() => res.json('Events updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+    });
+
+    //delete event
     router.route('/:id').delete((req, res) => {
         Events.findByIdAndDelete(req.params.id)
         .then(events => res.json("Event deleted"))
         .catch(err => res.status(400).json('Error: ' + err));
     });
 
-    router.route('/update/:id').post((req, res) => {
-        Events.findById(req.params.id)
-            .then(events => {
-            events.eventsname = req.body.eventsname;
-            events.description = req.body.description;
-            events.price = Number(req.body.price);
-        
-            events.save()
-                .then(() => res.json('Events updated!'))
-                .catch(err => res.status(400).json('Error: ' + err));
-            })
-            .catch(err => res.status(400).json('Error: ' + err));
-    });
-
-
+    
 module.exports = router;
--- balloon-reactapp/backend/server.js
+++ balloon-reactapp/backend/server.js
@@ -13,23 +13,21 @@ app.use(cors());
 app.use(express.json()); 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri
-);
+mongoose.connect(uri);
 
 const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log("MongoDB database connection established successfully");
 })
 
-//var CONNECTION_STRING ="mongodb+srv://ziweihu98:6991591@bostonggevents.u1njw0p.mongodb.net/?retryWrites=true&w=majority";
-
 const eventsRouter = require('./routes/events');
 const usersRouter = require('./routes/users');
+
 
 app.use('/events', eventsRouter);
 app.use('/users', usersRouter);
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('server is running');
 })
 
