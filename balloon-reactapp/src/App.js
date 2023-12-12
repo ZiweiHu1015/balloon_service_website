6e33002074c3cf4aca107477a7f48b740e6ee80d
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2023-12-12 14:34:22 -0500

    ziwei change home router

--- balloon-reactapp/src/App.js
+++ balloon-reactapp/src/App.js
@@ -3,12 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
 import Footer from './components/Footer';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './components/Home';
+import Events from './components/Events';
+import Balloon from './components/Balloon';
+import Flower from './components/Flower';
+import GrabAndGo from './components/GrabAndGo';
+
 function App() {
   return (
+    <Router>
     <div className="App">
      <NavbarComp/>
-     <Footer/>
+        <Routes>
+            <Route path = "/" element = {<Home/>} />
+            <Route path = "/events" element = {<Events/>} />
+            <Route path = "/balloon" element = {<Balloon/>} />
+            <Route path = "/flower" element = {<Flower/>} />
+            <Route path = "/grabAndGo" element = {<GrabAndGo/>}/>
+        </Routes>
     </div>
+     <Footer/>
+ 
+    </Router>
   );
 }
 
