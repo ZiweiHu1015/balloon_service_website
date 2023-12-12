b9fd4c5fdb5570d780990fcf563f04229994155e
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2023-12-12 17:06:58 -0500

    ziwei test

--- balloon-reactapp/backend/routes/events.js
+++ balloon-reactapp/backend/routes/events.js
@@ -1,58 +1,27 @@
-
 const router = require('express').Router();
-let Events = require('../models/events.model');//need to require the model
+let Event = require('../models/event.model');
 
-    //get all events 
-    router.route('/').get((req,res) =>{
-        Events.find() //find all events in database
-            .then(events => res.json(events))
-            .catch(err => res.status(400).json('Error: '+err));
-    });//first route handles http get request
-
-    //get event by ID
-    router.route('/:id').get((req, res) => {
-        Events.findById(req.params.id)
+router.route('/').get((req,res)=> {
+    Event.find() //get a list of all events
         .then(events => res.json(events))
-        .catch(err => res.status(400).json('Error: ' + err));
-    });
+        .catch(err => res.status(400).json('Error: ' + err ));
+});
 
-    //create new event
-    router.route('/add').post((req,res) =>{
-        const eventname = req.body.eventname;
-        const description = req.body.description;
-        const price = Number(req.body.price);
-
-        const newEvents = new Events({
-            eventname,  
-            description,
-            price,
-        });
-    newEvents.save()
-        .then(()=> res.json('Events added!'))
-        .catch(err => res.status(400).json('Error: '+ err));
-    });//second route handles http post request
-
-    //update event
-    router.route('/update/:id').post((req, res) => {
-        Events.findById(req.params.id)
-        .then(events => {
-        events.eventsname = req.body.eventsname;
-        events.description = req.body.description;
-        events.price = Number(req.body.price);
+router.route('/add').post((re, res) =>{
+    const eventname = req.body.eventname;
+    const description = req.body.description;
+    const price = Number(req.body.price);
     
-        events.save()
-            .then(() => res.json('Events updated!'))
-            .catch(err => res.status(400).json('Error: ' + err));
-        })
-        .catch(err => res.status(400).json('Error: ' + err));
+    const newEvent = new Event({
+        eventname, 
+        description,
+        price
     });
-
-    //delete event
-    router.route('/:id').delete((req, res) => {
-        Events.findByIdAndDelete(req.params.id)
-        .then(events => res.json("Event deleted"))
-        .catch(err => res.status(400).json('Error: ' + err));
-    });
-
     
+    newEvent.save()//saved to database
+        .then(() => res.json('Event added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
 module.exports = router;
