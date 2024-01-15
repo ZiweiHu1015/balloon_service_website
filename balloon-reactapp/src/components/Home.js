a1a170fb73a11e592c9b2fc085b9fa9459563fb3
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2024-01-14 23:20:17 -0500

    small fix

--- balloon-reactapp/src/components/Home.js
+++ balloon-reactapp/src/components/Home.js
@@ -4,9 +4,9 @@ import '../App.css';
 import logo from './images/logo/logo.png';
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
-import EncorePink from './images/balloons/EncorePinkOne.jpg';
-import Davios_seaport from './images/balloons/Davios_seaport.jpg';
-import NH_heart1 from './images/balloons/NH_heart1.PICT';
+import EncoreRed from './images/balloons/encoreRed.jpg';
+import Empire from './images/balloons/empire.jpg';
+import Jiangnan from './images/balloons/jiangnan.jpg';
 import EncoreBlack from './images/balloons/EncoreBlack.jpg';
 import tallCenterPiece from './images/flowers/tallCenterPiece.jpg';
 import happyBDay from './images/grab_and_go/happyBDay.jpg'
@@ -18,29 +18,31 @@ export default class Home extends Component {
       <main>
         
       <Container>
-        {/*logo*/}
         
         {/*Carousel pictures*/}
+
+        <div className = "carousel-frame">
           <Carousel >
             <Carousel.Item>
-              <Image src = "https://i.postimg.cc/KvhGdtLC/IMG-1093.jpg" fluid />
+              <Image src = {Empire} fluid />
               <Carousel.Caption>
-              </Carousel.Caption>
+              </Carousel.Caption> 
             </Carousel.Item>
 
             <Carousel.Item>
-              <Image src = "https://i.postimg.cc/GpQwQ7WZ/IMG-0893.jpg" fluid />
+              <Image src = {Jiangnan} fluid />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
             
             <Carousel.Item>
-              <Image src = "https://i.postimg.cc/Sx3gCW1c/IMG-1001.jpg" fluid />
+              <Image src = {EncoreRed} fluid />
               <Carousel.Caption>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
-         
+        </div>
+     
 
       {/*intro text*/}
       <div style={{ margin: '50px' }} >
@@ -62,7 +64,7 @@ export default class Home extends Component {
    
 
       {/*cards for display*/}
-      <div classNme ="dark-gray-text">
+      <div className ="dark-gray-text">
       <Row className="mb-3">
         <Col md={6} className="d-flex align-items-stretch">
           <img src={EncoreBlack} alt="Onsite Installation" style={{ width: '100%' }} />
@@ -71,7 +73,7 @@ export default class Home extends Component {
           <div>
             <h3 style={{margin: '20px',fontWeight: 'bold'}}>Onsite Installation</h3>
             <p style={{ color: '#757575', margin: '20px'}}>
-            Based in North Quincy, MA, we provide onsite balloon installations in the greater Boston region, 
+            Based in North Quincy, MA, we provide onsite balloon and flower decoration service in the greater Boston region, 
             including but not limited to Boston, Cambridge, Allston, Brookline, Brighton, Somerville, Waltham, Newton, 
             and Lexington. Our on-site installation services start from $299, depending on the design and location. 
             This price includes setup, delivery, and balloon designs.
